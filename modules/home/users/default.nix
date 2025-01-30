b3d3524e9aaa85bd0104a7a552f275c7909a6249
Chris Denneen <cdenneen@gmail.com>
2025-01-29 22:06:48 -0500

    Adding nix-community cachix

--- modules/home/users/default.nix
+++ modules/home/users/default.nix
@@ -65,8 +65,8 @@ in
     xdg.configFile = {
       "nix/nix.conf".text = ''
         experimental-features = nix-command flakes
-        substituters = https://cache.nixos.org https://cosmic.cachix.org https://cdenneen.cachix.org
-        trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE= cdenneen.cachix.org-1:EUognwSf1y0FAzDOPmUuYtz6aOxCWyNbcMi8PjHV8gU=
+        substituters = https://cache.nixos.org https://nix-community.cachix.org https://cosmic.cachix.org https://cdenneen.cachix.org
+        trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE= cdenneen.cachix.org-1:EUognwSf1y0FAzDOPmUuYtz6aOxCWyNbcMi8PjHV8gU=
       '';
       "nixpkgs/config.nix".text = ''
         {
--- modules/os/default.nix
+++ modules/os/default.nix
@@ -38,11 +38,13 @@ in
         substituters = config.nix.settings.trusted-substituters;
         trusted-substituters = [
           "https://cache.nixos.org"
+          "https://nix-community.cachix.org"
           "https://cosmic.cachix.org"
           "https://cdenneen.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
           "cdenneen.cachix.org-1:EUognwSf1y0FAzDOPmUuYtz6aOxCWyNbcMi8PjHV8gU="
         ];
