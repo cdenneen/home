7574d9ad511da9cfd00eeab875c8b1a7bfa4da9c
Chris Denneen <cdenneen@gmail.com>
2026-02-05 13:08:32 -0500

    Add glab

--- modules/home/users/cdenneen/programs.nix
+++ modules/home/users/cdenneen/programs.nix
@@ -16,6 +16,7 @@
     kubectl
     kubernetes-helm
     kubeswitch
+    glab
 
     # Runtimes / cloud
     nodejs_20 # LTS
@@ -30,5 +31,37 @@
     direnv.enable = true;
     direnv.nix-direnv.enable = true;
     starship.enable = true;
+    # glab config is managed explicitly via home.file
   };
+
+  home.file.".config/glab-cli/config.yml".text = ''
+    git_protocol: ssh
+    editor:
+    browser:
+    glamour_style: dark
+    check_update: true
+    display_hyperlinks: false
+    host: git.ap.org
+    no_prompt: false
+    telemetry: false
+
+    hosts:
+      gitlab.com:
+        api_protocol: https
+        git_protocol: ssh
+        user: cdenneen
+        container_registry_domains:
+          - gitlab.com
+          - gitlab.com:443
+          - registry.gitlab.com
+
+      git.ap.org:
+        api_protocol: https
+        git_protocol: ssh
+        user: cdenneen
+        container_registry_domains:
+          - git.ap.org
+          - git.ap.org:443
+          - registry.associatedpress.com
+  '';
 }
