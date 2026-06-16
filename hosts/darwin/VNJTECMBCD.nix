3feef9e37d7f7a7046d04192b12b8ff54813c845
Chris Denneen <cdenneen@gmail.com>
2026-06-16 14:33:42 -0400

    jarvis: use darwin module

--- hosts/darwin/VNJTECMBCD.nix
+++ hosts/darwin/VNJTECMBCD.nix
@@ -1,5 +1,12 @@
-{ lib, pkgs, ... }:
 {
+  jarvis,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [ jarvis.darwinModules.jarvis-node ];
+
   networking.hostName = "VNJTECMBCD";
 
   system.stateVersion = 6;
@@ -55,23 +62,10 @@
     pkgs.uv
   ];
 
-  launchd.daemons.jarvis-node = {
-    script = ''
-      set -euo pipefail
-
-      export HOME="/Users/cdenneen"
-      export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/homebrew/bin:${pkgs.uv}/bin"
-
-      exec ${pkgs.uv}/bin/uv tool run --from jarvis==0.1.0a3 jarvis-node
-    '';
-    serviceConfig = {
-      RunAtLoad = true;
-      KeepAlive = true;
-      StandardOutPath = "/Users/cdenneen/Library/Logs/jarvis-node.log";
-      StandardErrorPath = "/Users/cdenneen/Library/Logs/jarvis-node.log";
-      ProcessType = "Background";
-      UserName = "cdenneen";
-    };
+  services.jarvis-node = {
+    enable = true;
+    mode = "python";
+    python.package = "jarvis==0.1.0a3";
   };
 
 }
