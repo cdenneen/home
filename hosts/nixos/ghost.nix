7801a9737495d5010ea88da436aae5edfb76b9fc
Chris Denneen <cdenneen@gmail.com>
2026-06-23 23:12:38 -0400

    ghost: drop ipv6 loopback binds

--- hosts/nixos/ghost.nix
+++ hosts/nixos/ghost.nix
@@ -158,14 +158,13 @@ in
         local all postgres peer
         local all all scram-sha-256
         host all all 127.0.0.1/32 scram-sha-256
-        host all all ::1/128 scram-sha-256
       '';
     };
 
     redis.servers."" = {
       enable = true;
       port = redisPort;
-      bind = "127.0.0.1 ::1";
+      bind = "127.0.0.1";
       requirePassFile = redisPasswordFile;
       settings = {
         dir = redisDataDir;
