d3dabdb6552d738c89ed145b15d9295a57873c87
Chris Denneen <cdenneen@gmail.com>
2026-07-31 13:15:44 -0500

    nixos: format open-webui container config

--- modules/system/nixos/containers/chat.nix
+++ modules/system/nixos/containers/chat.nix
@@ -26,20 +26,19 @@ in
 
   config = lib.mkIf cfg.enable {
     containerPresets.podman.enable = lib.mkDefault true;
-    virtualisation.arion.projects.open-webui.settings.services.open-webui.service =
-      {
-        image = "ghcr.io/open-webui/open-webui:v0.11.0@sha256:72c0ba641ba75e7aa52655cb242570906ececd09b1140fb736483038a22b3228";
-        ports = lib.optionals (!useHostNetwork) [ "${toString cfg.port}:8080" ];
-        volumes = [
-          "${cfg.dataDir}:/app/backend/data"
-        ];
-        environment = {
-          OLLAMA_BASE_URL = cfg.ollamaBaseUrl;
-        };
-      }
-      // lib.optionalAttrs useHostNetwork {
-        network_mode = "host";
+    virtualisation.arion.projects.open-webui.settings.services.open-webui.service = {
+      image = "ghcr.io/open-webui/open-webui:v0.11.0@sha256:72c0ba641ba75e7aa52655cb242570906ececd09b1140fb736483038a22b3228";
+      ports = lib.optionals (!useHostNetwork) [ "${toString cfg.port}:8080" ];
+      volumes = [
+        "${cfg.dataDir}:/app/backend/data"
+      ];
+      environment = {
+        OLLAMA_BASE_URL = cfg.ollamaBaseUrl;
       };
+    }
+    // lib.optionalAttrs useHostNetwork {
+      network_mode = "host";
+    };
     networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ cfg.port ];
   };
 }
